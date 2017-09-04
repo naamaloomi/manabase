8321239a586f90567e195d0af55f3de23056c3f7
Amir Sadeghin <amir.sadeghin@telecomputing.se>
2017-09-04 13:25:36 +0200

    Updated functions to be more standalone and assume less.

--- Manabase.py
+++ Manabase.py
@@ -9,52 +9,52 @@ def percentage(part, whole):
     return 100 * float(part) / float(whole)
 
 
-def sim_game_start(turns_allowed):
-    nr_cards = 60
-    nr_lands = 18
-    nr_good_lands = 16
-
-    # Shuffle deck
-    d = Deck(nr_cards, nr_lands, nr_good_lands)
-
+def start_sim(deck, turns_allowed):
     # Draw 7
-    h = d.draw_hand()
+    h = deck.draw_hand()
 
     # Decide mulligan
     if h.lands < 2 or h.lands > 5:
         # Mulligan to 6
-        h = d.mulligan()
-    if h.lands < 2 or h.lands > 4:
+        h = deck.mulligan()
+    if h.lands < 1 or h.lands > 5:
         # Mulligan to 5
-        h = d.mulligan()
-    if h.lands < 1 or h.lands > 4:
+        h = deck.mulligan()
+    # if h.lands < 1 or h.lands > 4:
         # Mulligan to 4
-        h = d.mulligan()
+        # h = deck.mulligan()
     # Draw cards for the number of turns left
     for i in range(1, turns_allowed + 1):
-        card = d.draw_card()
+        card = deck.draw_card()
         h.add_card(card)
 
     return h
 
 
-def get_good_land_prob():
-    nr_iterations = 100000
-    nr_good_lands_needed = 2
-    turns_allowed = 1
+def get_good_land_prob(nr_cards=60,
+                       nr_lands=18,
+                       nr_good_lands=13,
+                       nr_iterations=100000,
+                       nr_good_lands_needed=1,
+                       turns_allowed=0):
     success = 0
 
     for i in range(0, nr_iterations):
-        h = sim_game_start(turns_allowed)
+        # Make deck
+        d = Deck(nr_cards, nr_lands, nr_good_lands)
+
+        # Start simulation
+        h = start_sim(d, turns_allowed)
+
+        # Evaluate result
         if h.good_lands >= nr_good_lands_needed:
             success += 1
 
-    return 'Prob. = {p}%, ' \
-           'Success = {s}, ' \
-           'Iter. = {i}.'.format(
+    return 'Probability to get {gl} good land in {t} turns = {p}%, ' \
+           ''.format(
+                gl=nr_good_lands_needed,
+                t=turns_allowed,
                 p=percentage(success, nr_iterations),
-                s=success,
-                i=nr_iterations
             )
 
 
